6bac4018326529dc31cfaa7b8e3599f74f67c6fa
santanamateus02 <santanamateus02@gmail.com>
2026-01-09 21:57:54 -0300

    primeiro commit

---
+++ src/main/java/com/coupon/cupom/CupomApplication.java
@@ -0,0 +1,13 @@
+package com.coupon.cupom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupomApplication.class, args);
+	}
+
+}
